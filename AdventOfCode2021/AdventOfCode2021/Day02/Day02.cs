ea2d29557488b7023a91780d474db62350775243
Colin DeClue <cdeclue@rightpoint.com>
2021-12-02 11:23:05 -0600

    day 2

---
+++ AdventOfCode2021/AdventOfCode2021/Day02/Day02.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace AdventOfCode2021.Day02;
+
+public class Day02 : BaseDay
+{
+    public override string GetPart1()
+    {
+        var startX = 0;
+        var startY = 0;
+        var commands = GetInputFromFile().Split(Environment.NewLine).Select(line => line.Split(" ")).ToArray();
+        foreach (var command in commands)
+        {
+            var distance = int.Parse(command[1]);
+            var _ = command[0] switch
+            {
+                "forward" => startX += distance,
+                "down" => startY += distance,
+                "up" => startY -= distance,
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        return (startX * startY).ToString();
+    }
+
+    public override string GetPart2()
+    {
+        var startX = 0;
+        var startY = 0;
+        var startAim = 0;
+        var commands = GetInputFromFile().Split(Environment.NewLine).Select(line => line.Split(" ")).ToArray();
+        foreach (var command in commands)
+        {
+            var distance = int.Parse(command[1]);
+            switch (command[0])
+            {
+                case "forward":
+                    startX += distance;
+                    startY += startAim * distance;
+                    break;
+                case "down":
+                    startAim += distance;
+                    break;
+                case "up":
+                    startAim -= distance;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        return (startX * startY).ToString();
+    }
+}
+
--- AdventOfCode2021/AdventOfCode2021/Program.cs
+++ AdventOfCode2021/AdventOfCode2021/Program.cs
@@ -1,5 +1,5 @@
-﻿using AdventOfCode2021.Day01;
+﻿using AdventOfCode2021.Day02;
 
-var day = new Day01();
+var day = new Day02();
 
 Console.WriteLine(day.GetPart2());
